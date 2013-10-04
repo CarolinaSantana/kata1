3a0c4349c602bc625bcf1a7925b3b51e88446e3a
usuario <usuario@10.22.146.198>
2013-10-04 14:48:57 +0100

    secondCommit

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -4,8 +4,7 @@ import java.util.Date;
 public class Kata1 {
 
     public static void main(String[] args) {
-        Student student = new Student ("Carolina", new Date (93,4,10));
+        Student student = new Student ("Ana", new Date (93,4,10));
         System.out.println (student.getAge()); 
-        
     }
 }
